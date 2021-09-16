ea0a59e8acc116c88a4b7f717bbe27d601803907
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-09-16 09:51:05 +0200

    add todo card

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, Container, IconButton, Typography } from '@material-ui/core'
-import { Check } from '@material-ui/icons'
+import { Check, Delete } from '@material-ui/icons'
 import React from 'react'
 
 const Todo = () => {
@@ -9,12 +9,17 @@ const Todo = () => {
         <Card variant='outlined' style={{ marginTop: 35, background: 'lightgray' }}>
           <CardContent>
             <Typography variant='h5' component='h2'>
-              <IconButton><Check style={{ color: 'green' }} /></IconButton>
+              <IconButton>
+                <Check style={{ color: 'green' }} />
+              </IconButton>
+              Todo 1
+              <IconButton style={{ float: 'right' }}>
+                <Delete style={{ color: 'red' }} />
+              </IconButton>
             </Typography>
           </CardContent>
         </Card>
       </Container>
-
     </div>
   )
 }
